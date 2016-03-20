d3b467ffc4a32da1f6e1a742c52e29d15a36afbc
Katrina Hall <katrina@Katrinas-MBP.home>
2016-03-20 12:07:12 -0400

    Added lein-ring plugin for reloading.

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler clojure-todo-list.core/handler}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]]
